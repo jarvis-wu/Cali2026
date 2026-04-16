4a438d4585e1e6105ebefb5ec72ac4a7bdfcadec
Jarvis Wu <jarviszwu@gmail.com>
2026-04-16 12:46:21 -0400

    Set base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import mdx from '@astrojs/mdx';
 // Set site to your GitHub Pages URL: https://<your-username>.github.io/<repo-name>
 // If deploying to Netlify/Vercel, you can remove the site and base fields.
 export default defineConfig({
-  // site: 'https://YOUR_USERNAME.github.io',
-  // base: '/cali2026',
+  site: 'https://jarvis-wu.github.io',
+  base: '/Cali2026',
   integrations: [mdx()],
 });
